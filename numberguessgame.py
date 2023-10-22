66641bd4cd28101ce36846f644d2402d8c4361fd
Jay9301 <148665207+Jay9301@users.noreply.github.com>
2023-10-22 19:04:53 +0100

    numberguessgame.py

--- numberguessgame.py
+++ numberguessgame.py
@@ -9,26 +9,26 @@ def play_number_guessing_game():
   The user has 5 guesses to guess a randomly generated number between 1 and 10.
   """
 
-  # Generate a random number
+  # below will create a random number 
   random_number = generate_random_number()
 
-  # Initialize the number of guesses
+  # select the the number of guesses
   number_of_guesses = 0
 
-  # Print the introductory message
+  # this is the introductory message I have added in brand new. 
   print("Welcome to the Guess the Number game! You will have 5 chances to guess the correct number.")
 
-  # Start the game loop
+  # will loop the game 5 times for time saving purposes
   while number_of_guesses < 5:
     # Get the user's guess
     guess = int(input("Guess a number between 1 and 10: "))
 
-    # Check if the guess is correct
+    #  the below code will confirm if the guess is correct or not
     if guess == random_number:
       print("You guessed correctly! Congratulations!")
       break
 
-    # Check if the guess is too high or too low
+    # Check if the guess is too high/low - help the end user 
     elif guess > random_number:
       print("Your guess is too high.")
     else:
@@ -37,7 +37,7 @@ def play_number_guessing_game():
     # Increment the number of guesses
     number_of_guesses += 1
 
-  # If the user runs out of guesses, tell them the correct answer
+  # If the user runs out of guesses, the below code will tell the user the correct answer
   if number_of_guesses == 5:
     print("You ran out of guesses. The correct answer was", random_number)
 
